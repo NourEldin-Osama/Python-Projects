b511a9a59932704512e4b601069a71c11eb4e43d
NOURELDIN OSAMA <58091991+NourEldin-Osama@users.noreply.github.com>
2021-06-04 02:20:56 +0200

    Update the File

--- Get Public IP/GET Public IP address From CMD.py
+++ Get Public IP/GET Public IP address From CMD.py
@@ -1,1 +1,16 @@
 import os
+
+# WAY 1
+print(os.popen('curl ifconfig.me').read())
+
+# WAY 2
+print(os.popen('curl ifconfig.me/ip').read())
+
+# WAY 3
+print(os.popen('curl ifconfig.me/all').read().split("\n")[0].split(" ")[1])
+
+# WAY 4
+print(os.popen('curl "http://myexternalip.com/raw"').read())
+
+# WAY 5
+print(os.popen('nslookup myip.opendns.com resolver1.opendns.com').read().split("\n")[-3].split(" ")[-1])
