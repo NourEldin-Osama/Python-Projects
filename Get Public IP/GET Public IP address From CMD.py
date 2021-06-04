b3d9e849c437b5eb7f5585789c0f8b702d484e86
NOURELDIN OSAMA <58091991+NourEldin-Osama@users.noreply.github.com>
2021-06-04 02:27:27 +0200

    Update GET Public IP address From CMD.py

--- Get Public IP/GET Public IP address From CMD.py
+++ Get Public IP/GET Public IP address From CMD.py
@@ -1,4 +1,7 @@
 import os
+from requests import get
+
+# Get External IP address / Public IP
 
 # WAY 1
 print(os.popen('curl ifconfig.me').read())
@@ -14,3 +17,9 @@ print(os.popen('curl "http://myexternalip.com/raw"').read())
 
 # WAY 5
 print(os.popen('nslookup myip.opendns.com resolver1.opendns.com').read().split("\n")[-3].split(" ")[-1])
+
+# WAY 6
+print('My public IP address is:', get('https://api.ipify.org').text)
+
+# WAY 7
+print('My public IP address is:', get('https://icanhazip.com/').text.strip())
