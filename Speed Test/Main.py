76408d55cc72612be2e94fc70fee2ded09886c27
NOURELDIN OSAMA <58091991+NourEldin-Osama@users.noreply.github.com>
2021-06-05 18:30:14 +0200

    Create Main.py for Speed Test Program
    Make sure to run "pip install -r requirements.txt" in your shell

---
+++ Speed Test/Main.py
@@ -0,0 +1,18 @@
+# Speed Test Program
+# First You should run "pip install -r requirements.txt" in your shell
+import os
+
+output = os.popen("speedtest-cli").read().split("\n")
+if len(output) == 2:
+    print("Warning")
+    print("No Internet Connection, Make Sure There Is An Internet Connection And Try Again.")
+else:
+    isp = output[1][13:output[1].find("(") - 1]  # Isp
+    ip = output[1].split("(")[1][:-4]  # Ip
+    host = output[4].split(": ")[0]  # Host
+    ping = output[4].split(": ")[1]  # Ping
+    down_speed = output[6]  # Download Speed
+    upload_speed = output[8]  # Upload Speed
+
+    print("Test Completed")
+    print("ISP: " + isp, "IP: " + ip, host, "Ping: " + ping, down_speed, upload_speed, sep='\n')
